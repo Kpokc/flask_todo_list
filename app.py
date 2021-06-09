5f545ef79074034d1bd72c7a9b9d49453055344e
Kpokc <pavmakarovs@gmail.com>
2021-06-09 22:15:14 +0000

    Add register functionality

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask, render_template, redirect, request, session, url_for
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -11,7 +13,7 @@ app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.secret_key = os.environ.get("SEKRET_KEY")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
@@ -25,6 +27,24 @@ def get_tasks():
 
 @app.route("/register", methods={"GET", "POST"})
 def register():
+    if request.method == "POST":
+        # check if user already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists!")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        #put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successfull!")        
     return render_template("register.html")
 
 
