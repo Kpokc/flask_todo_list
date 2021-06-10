9b5267ec3bcbef2ea013d3a326bee449a6e81f25
Kpokc <pavmakarovs@gmail.com>
2021-06-10 11:01:52 +0000

    Add categories dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,10 @@ $(document).ready(function(){
   $('.sidenav').sidenav({edge: "right"});
   $('.collapsible').collapsible();
   $('.tooltipped').tooltip();
+  $('select').formSelect();
   $('.datepicker').datepicker({
     format: "dd mmmm, yyyy",
-    yearRange: +3,
+    yearRange: 3,
     showClearBtn: true,
     i18n: {
       done: "Select"
